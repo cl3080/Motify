6d07181d95f3f1b9f1c0273a99117e25c12c3f54
lanking520 <lanking520@gmail.com>
2017-05-06 20:42:41 -0400

    add two user field

--- app/modules/shared/user/user.ts
+++ app/modules/shared/user/user.ts
@@ -3,5 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class User {
     public token: string;
+    public name:string;
+    public photo:string;
     public constructor() {}
 }

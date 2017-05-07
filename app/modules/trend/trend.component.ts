2e3449a351d5768840480f5ca9fa4c80ce6c1f1b
lanking520 <lanking520@gmail.com>
2017-05-07 11:50:40 -0400

    Add Get trend Movie APIs

--- app/modules/trend/trend.component.ts
+++ app/modules/trend/trend.component.ts
@@ -1,10 +1,22 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-
+import { Http, Headers, Response, URLSearchParams} from '@angular/http';
+import { Config } from '../shared/config';
 @Component({
   selector: 'trend',
   templateUrl: 'modules/trend/trend.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TrendComponent {
+  constructor(private http: Http){}
   text: string = 'Trend Page';
+  ngOnInit() {
+    let popularparam : URLSearchParams = new URLSearchParams();
+    popularparam.set("api_key",Config.TMDBAPIKEY);
+    popularparam.set("language","en-US");
+    this.http.get(Config.GetPopularMovie,{search: popularparam})
+    .map(res => res.json())
+    .subscribe((response: any) => {
+      console.log(JSON.stringify(response));
+    });
+  }
 }

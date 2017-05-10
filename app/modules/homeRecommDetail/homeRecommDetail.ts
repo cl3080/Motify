fcbd5ce9be8f87862fdc42f2b09f29e515c10643
tchaikyu <yq2212@columbia.edu>
2017-05-09 23:51:56 -0400

    page post complete

--- app/modules/homeRecommDetail/homeRecommDetail.ts
+++ app/modules/homeRecommDetail/homeRecommDetail.ts
@@ -1,6 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MovieDetailPage } from '../shared/moviedetailpage/moviedetailpage';
 // var imageSource = require("image-source");
+import { Http, Headers, Response, URLSearchParams, RequestOptions} from '@angular/http';
+import {Config} from "../shared/config";
+import {User} from "../shared/user/user";
+
 
 
 @Component({
@@ -11,12 +15,28 @@ import { MovieDetailPage } from '../shared/moviedetailpage/moviedetailpage';
 })
 export class HomeRecommDetailComponent {
 
-    public constructor(private movieRecommDetail: MovieDetailPage){
+    public constructor(private movieRecommDetail: MovieDetailPage, private http: Http, private user: User){
         // console.log(this.movieRecommDetail.title);
     }
 
     ngOnInit() {
         // this.userService.getUserPhoto(this.user);
+        this.postRating();
+
     }
-    text: string = 'Home Recomm Page';
+
+    postRating() {
+        console.log("sending movie detail info");
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        let body = JSON.stringify({UserId: this.user.id, MovieTMDBId: this.movieRecommDetail.id});
+        console.log("posting body is " + body);
+        let options = new RequestOptions({headers: headers, method:"post"});
+        console.log("posting...");
+        this.http.post(Config.PostSearchItemUrl, body, options)
+            .map(res => res.json())
+            .subscribe((response: any) => {
+                console.log("responding message is " + JSON.stringify(response))
+            });
+    }
 }
